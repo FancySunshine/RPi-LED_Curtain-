ded172257f073bd6e9fd131b82c5cb19161c1431
kimyoungchang <dudckd6826@github.com>
2021-09-18 18:13:21 +0900

    curtaint_led mod

--- Curtain_LED.py
+++ Curtain_LED.py
@@ -1,7 +1,7 @@
 import time
 import RPi.GPIO as GPIO
-#from multiprocessing import Process, Value
-#import keyboard
+# from multiprocessing import Process, Value
+# import keyboard
 import paho.mqtt.client as mqtt
 import board  # Simple test for NeoPixels on Raspberry Pi
 import neopixel
@@ -10,25 +10,63 @@ import adafruit_tsl2561
 from micropython import const
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.base import JobLookupError
-#import VL53L0X
+# import VL53L0X
 import serial
-
+import threading
+import time
 
 # 시리얼 통신에 관한 설정
 
-"""
+
 port = '/dev/ttyACM0'
 brate = 9600
-auto = False
-seri = serial.Serial(port, baudrate = brate, timeout = None)
+seri = serial.Serial(port, baudrate=brate, timeout=None)
 print(seri.name)
-"""
 
 # LED의 초기 색상과 밝기 설정
 # 커튼의 초기 단계 설정
 n = 0
 rgb = [0, 230, 50]
 brightness = 0
+
+auto = False
+personIsIn = False
+
+
+class SerialThread(threading.Thread):
+
+    def __init__(self, name):
+        super().__init__()
+        self.name = name  # thread 이름 지정
+
+    def run(self):
+        global auto, personIsIn, seri
+        while True:
+            if auto:
+                while True:
+                    if seri.in_waiting != 0:
+                        content = seri.readline()
+                        content = content[:3].decode().strip()
+                        print(content)
+                        if content == "In":
+                            personIsIn = True
+                            break
+                        elif content == "Out":
+                            personIsIn = False
+                            break
+                        
+
+                    
+                if personIsIn:
+                    print("PERSON IN")
+                else:
+                    print("PERSON OUT")
+
+
+t = SerialThread("arduinoThread")
+t.daemon = True
+t.start()
+
 
 # mqtt 연결 설정
 def on_connect(client, userdata, rc, properties=None):
@@ -37,11 +75,12 @@ def on_connect(client, userdata, rc, properties=None):
     client.subscribe("auto/ctr")
     client.subscribe("led/color")
     client.subscribe("led/bright")
+    client.subscribe("auto/result")
+
 
 # mqtt 토픽에 의한 행동 설정
 def on_message(client, userdata, msg):
-    global n
-    global led
+    global n, led, auto, seri, personIsIn
 
     if msg.topic == "ctn/step":
         n = int(msg.payload)
@@ -53,18 +92,45 @@ def on_message(client, userdata, msg):
         print("asdfasfsdfBifdfasdfsdf")
         led.setBright()
     elif msg.topic == "auto/ctr":
-        if msg.payload == "0":
+        if int(msg.payload) == 0:
+            seri.write("0".encode())
+            print('AUTOMODE OFF')
             auto = False
         else:
+            seri.write("1".encode())
+            print('AUTOMODE ON')
             auto = True
-        #auto_detection()
-    print(msg.topic + " " + str(msg.payload))
+    elif msg.topic == "auto/result":
+        print(auto)
+        print(personIsIn)
+        if auto and personIsIn:
+            result = eval(msg.payload.decode('utf-8'))
+            if "curtain" in result:
+                if result["curtain"] == "up":
+                    if n < 4:   
+                        n += 1
+                elif result["curtain"] == "down":
+                    if n > 0:
+                        n -= 1
+            elif "led" in result:
+                if result["led"] == "up":
+                    if led.bright < 125:
+                        led.bright += 0.05 * 1.25
+                        led.setBright()
+                elif result["led"] == "down":
+                    if led.bright > 0:
+                        led.bright -= 0.05 * 1.25
+                        led.setBright()
+
+    # auto_detection()
+    # print(msg.topic + " " + str(msg.payload))
+
 
 client = mqtt.Client(client_id="asdf")
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.120.158", 1883, 60)
+client.connect("192.168.192.158", 1883, 60)
 client.loop_start()
 
 # 센서 사용에 팔요한 GPIO 설정
@@ -74,13 +140,14 @@ GPIO.setmode(GPIO.BCM)
 pixel_pin = board.D18
 # 픽셀 수 (24구)
 num_pixels = 24
-#색상 순서 Green, Red, Blue
+# 색상 순서 Green, Red, Blue
 ORDER = neopixel.GRB
 
 # LED의 기본설정
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=brightness, auto_write=False, pixel_order=ORDER
 )
+
 
 class LED:
     def __init__(self, num_pixels, pixel_pin, bright, rgb):
@@ -96,7 +163,7 @@ class LED:
     def setBright(self):
         pixels.brightness = led.bright
         pixels.show()
-        
+
 
 # curtain
 class step:
@@ -111,9 +178,9 @@ class step:
             GPIO.setup(pin, GPIO.OUT)
             GPIO.output(pin, False)
 
-# motor의 up 시퀀스
+    # motor의 up 시퀀스
     def up(self):
-#       up = [[0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0], [1, 0, 0, 0]]
+        #       up = [[0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0], [1, 0, 0, 0]]
 
         up = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
         try:
@@ -124,14 +191,14 @@ class step:
                         GPIO.output(xpin, True)
                     else:
                         GPIO.output(xpin, False)
-               	self.status -= 1
+                self.status -= 1
             time.sleep(0.001)
         except KeyboardInterrupt:
             print()
 
-#       print(self.status.value)
+    #       print(self.status.value)
 
-# motor 의 down 시퀀스
+    # motor 의 down 시퀀스
     def down(self):
         down = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
         try:
@@ -153,6 +220,8 @@ class step:
             self.down()
         else:
             self.up()
+
+
 # step class final
 
 def control(steps):
@@ -166,30 +235,10 @@ def control(steps):
             else:
                 step_arr[i].ctr(0)
 
+
 # get Lux from sensor
 # 자동 감지 함수, getlux(아두이노)
-"""
-def auto_detection():
-        a = 0
-        if(auto):
-                seri.write(auto.encode())
-                a=1
-                while a:
-                        if seri.in_waiting !=0 :
-                                content = seri.readline()
-                                print(content[:2].decode())
-                                a = 0
-getLux():
 
-        a = 0
-        seri.write(lux.encode())
-        a = 1
-        while a:
-                if seri.in_waiting != 0:
-                        content = seri.readline()
-                        print(content[:2].decode())
-                        a = 0
-"""
 
 def getLux():
     now = time.localtime()
@@ -198,7 +247,7 @@ def getLux():
     for i in tsl:
         lux = None
         while lux == None:
-            #print("Chip ID = {}".format(i.chip_id))
+            # print("Chip ID = {}".format(i.chip_id))
 
             # Enable the light sensor
             i.enabled = True
@@ -210,7 +259,7 @@ def getLux():
             # Set integration time (0=13.7ms, 1=101ms, 2=402ms, or 3=manual)
             i.integration_time = 1
 
-            #print("Getting readings...")
+            # print("Getting readings...")
 
             # Get raw (luminosity) readings individually    
 
@@ -221,20 +270,15 @@ def getLux():
 
             lux = i.lux
             print(lux)
-            i.enabled = False    
+            i.enabled = False
         lux = str(lux)
         lux_data.append(lux)
-        
-    
-        # Disble the light sensor (to save power)
+
+    # Disble the light sensor (to save power)
     lux_data = date + "|" + "|".join(lux_data)
-    print("--lux date, lux--")
-    
-    print(lux_data)
+    #print(lux_data)
     client.publish("Database/bright/save", lux_data)
-    
-    
-        
+
 
 # ---Lightsensor---
 # Create the I2C bus
@@ -250,9 +294,9 @@ print(tsl)
 
 # 스케쥴러 사용
 sched = BackgroundScheduler()
-sched.start()
 
 sched.add_job(getLux, 'cron', second='*/5', id='lux')
+sched.start()
 
 # motor가 연결된 핀 리스트
 pins1 = [4, 14, 15, 10]
@@ -281,11 +325,10 @@ step_arr = [step1, step2, step3, step4]
 try:
     while True:
         # mqtt client connect only network
-        client.loop()
+        #client.loop()
         control(n)
-        getLux()
-        print([i.status for i in step_arr])
-        
+        #print([i.status for i in step_arr])
+
 except KeyboardInterrupt:
     pixels.fill((0, 0, 0))
     pixels.show()
